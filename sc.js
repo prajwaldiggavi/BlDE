e1f87bc12f74e088753ab072e5ee898f8645aed5
prajwaldiggavi <153297020+prajwaldiggavi@users.noreply.github.com>
2025-01-30 00:53:17 +0530

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,87 +1,76 @@
-const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
-const nodemailer = require('nodemailer');
-const path = require('path');
-
-const app = express();
-const PORT = 3006;
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Set EJS as the view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));  // Make sure you have a 'views' folder
-
-// Serve static files (like CSS)
-app.use(express.static(path.join(__dirname, 'public')));
-
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Rahul@123',
-    database: 'studentdb'
-});
-
-db.connect((err) => {
-    if (err) {
-        console.error('Database connection failed:', err);
-        return;
-    }
-    console.log('Connected to MySQL database');
-});
-
-// Email Configuration
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'diggaviprajwal55@gmail.com',
-        pass: 'bgnm kbga xkew pgpb' // Use app-specific password
-    }
-});
-
-// Helper Function: Execute MySQL Queries
-const executeQuery = (query, params) => {
-    return new Promise((resolve, reject) => {
-        db.query(query, params, (err, results) => {
-            if (err) reject(err);
-            else resolve(results);
-        });
+$(document).ready(function() {
+    $('.carousel').slick({
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerPadding: '60px',
+        slidesToShow: 4,
+        responsive: [{
+            breakpoint: 2400,
+            settings: {
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },{
+            breakpoint: 1600,
+            settings: {
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 2
+            }
+        }, {
+            breakpoint: 880,
+            settings: {
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }, {
+            breakpoint: 768,
+            settings: {
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }]
     });
-};
 
-// Dashboard Route
-app.get('/dashboard', async (req, res) => {
-    try {
-        // Fetch total number of students
-        const totalStudents = await executeQuery('SELECT COUNT(*) AS total FROM students');
-        
-        // Fetch total attendance records
-        const totalAttendance = await executeQuery('SELECT COUNT(*) AS total FROM attendance');
+    $('#announcement-list').paginate({ itemsPerPage: 3 });
+    $('#news-list').paginate({ itemsPerPage: 3 });
+    $('#events-list').paginate({ itemsPerPage: 3 });
 
-        // Fetch subjects covered (distinct subject names)
-        const subjectsCovered = await executeQuery('SELECT COUNT(DISTINCT subjectName) AS total FROM attendance');
+    if ($('#announcement-list').length <= 3) {
+        $('#announcement-list-previous,#announcement-list-next').addClass('disabled');
+    }
 
-        // Fetch recent attendance records (last 5 attendance records)
-        const recentAttendance = await executeQuery(
-            'SELECT studentName, status, subjectName, date FROM attendance JOIN students ON attendance.roll_number = students.studentId ORDER BY date DESC LIMIT 5'
-        );
+    if ($('#events-list').children().length <= 3) {
+        $('#events-list-previous,#events-list-next').addClass('disabled');
+    }
 
-        // Render the dashboard
-        res.render('dashboard', {  // Ensure 'dashboard.ejs' exists inside the 'views' folder
-            totalStudents: totalStudents[0].total,
-            totalAttendance: totalAttendance[0].total,
-            subjectsCovered: subjectsCovered[0].total,
-            recentAttendance: recentAttendance
-        });
-    } catch (err) {
-        console.error('Error fetching dashboard data:', err);
-        res.status(500).send('Error fetching dashboard data');
+    if ($('#news-list').length <= 3) {
+        $('#news-list-previous,#news-list-next').addClass('disabled');
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+function contributors($arg){
+
+    if ($arg === undefined || $arg == '' || $arg == 'all')
+        console.log("%cContributors List\n\n"+"%c--Designed by--"+
+        "%c\nMofid Ansari (ansarimofid@gmail.com)\nPratyush Singh (singh.pratyush96@gmail.com)\n\n %c--Core Developers--\n%cMofid Ansari (ansarimofid@gmail.com)\nPratyush Singh (singh.pratyush96@gmail.com)\nSaurabh Jain (saurabhjn76@gmail.com)\n\n%c--Supporting Developers--\n%cRajuKoushik (g.rajukoushik@gmail.com)\nKamal Awasthi (kamalawasthi97@gmail.com)\nAvi Aryan (avi.aryan123@gmail.com)\n\n%c--Contribution through reporting issues--\n%cHarish Krupo (harishkrupo@gmail.com)\nShalinee Singh (shalinee43@gmail.com)\nMayank Maurya (mayankm522@gmail.com)\nNavneet Nandan (navneet.nandan8@gmail.com)\nPrem Chand Saini (premchandsaini779@gmail.com)\nUnni Rajendra (unni332265@gmail.com)\nRavi Kishan Jha (ravikishan77@gmail.com)\nAnkit Kumar (ankit.kumar071460@yahoo.com)",'color:rgb(163,169,72);text-align:center;font-size:2em','color: rgb(15,108,107);font-size:1.5em;text-align:center','color:rgb(50,50,50);font-size:1.0.5em','color: rgb(15,108,107);font-size:1.5em;','color:rgb(50,50,50);font-size:1.0.5em','color: rgb(15,108,107);font-size:1.5em;','color:rgb(50,50,50);font-size:1.0.5em','color: rgb(15,108,107);font-size:1.2em;','color:rgb(50,50,50);font-size:1.0.5em');
+    else if($arg == 'designer' || $arg =='design' || $arg =='designers')
+        console.log("%c--Designed by--%c\nMofid Ansari (ansarimofid@gmail.com)\nPratyush Singh (singh.pratyush96@gmail.com)",'color: rgb(15,108,107);font-size:1.5em;text-align:center','color:rgb(50,50,50);font-size:1.0.5em');
+    else if($arg == 'devs' || $arg == 'dev' || $arg == 'developers' || $arg == 'developer')
+    console.log("%c--Core Developers--\n%cMofid Ansari (ansarimofid@gmail.com)\nPratyush Singh (singh.pratyush96@gmail.com)\nSaurabh Jain (saurabhjn76@gmail.com)\n\n%c--Supporting Developers--\n%cRajuKoushik (g.rajukoushik@gmail.com)\nKamal Awasthi (kamalawasthi97@gmail.com)\nAvi Aryan (avi.aryan123@gmail.com)",'color: rgb(15,108,107);font-size:1.5em;','color:rgb(50,50,50);font-size:1.0.5em','color: rgb(15,108,107);font-size:1.5em;','color:rgb(50,50,50);');
+
+    return "Thank You";
+}
+
+console.log("Type %c contributors()%c to get list of contributors.\n %carguments(optional):all,devs,designers",'color:rgb(15,108,107)','color:rgb(50,50,50);','color:blue');
