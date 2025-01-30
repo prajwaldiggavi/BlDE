21c3201d839e5a106b2c672461501c252ae16ee4
prajwaldiggavi <153297020+prajwaldiggavi@users.noreply.github.com>
2025-01-31 04:01:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
-const cors = require('cors');  // Add this import
+const cors = require('cors');
 const app = express();
 const port = 8080;
 
