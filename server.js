c2ef1e5743edc01a983a3abf8bbdf8dd01fc7b11
prajwaldiggavi <153297020+prajwaldiggavi@users.noreply.github.com>
2025-02-01 11:43:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,31 +3,31 @@ const mysql = require('mysql');
 let dbConnection;
 
 function handleDisconnect() {
-    dbConnection = mysql.createConnection({
-        host: 'sql10.freesqldatabase.com',
-        user: 'sql10760370',
-        password: 'GUeSnpUSjf',
-        database: 'sql10760370',
-        port: 3306
-    });
+  dbConnection = mysql.createConnection({
+    host: 'sql10.freesqldatabase.com',
+    user: 'sql10760370',
+    password: 'GUeSnpUSjf',
+    database: 'sql10760370',
+    port: 3306  // MySQL server's default port
+  });
 
-    dbConnection.connect(function(err) {
-        if (err) {
-            console.error('Error connecting to database:', err);
-            setTimeout(handleDisconnect, 2000); // Reconnect after 2 seconds
-        } else {
-            console.log('Connected to database');
-        }
-    });
+  dbConnection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to database:', err);
+      setTimeout(handleDisconnect, 2000); // Try reconnecting after 2 seconds
+    } else {
+      console.log('Connected to database');
+    }
+  });
 
-    dbConnection.on('error', function(err) {
-        console.error('Database error:', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            handleDisconnect(); // Reconnect on lost connection
-        } else {
-            throw err;
-        }
-    });
+  dbConnection.on('error', (err) => {
+    console.error('Database error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect(); // Reconnect on connection loss
+    } else {
+      throw err;
+    }
+  });
 }
 
 handleDisconnect();
